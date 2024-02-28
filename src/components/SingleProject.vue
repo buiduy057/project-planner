5afa68604fc16b6509ca0e9202c4fe978297c7b3
bui duy 057 <buiduy057@gmail.com>
2024-02-28 22:20:14 +0700

    add

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="showDetail = !showDetail">
+        {{ project.title }}
+      </h3>
+    </div>
+    <div v-if="showDetail" class="detail">
+      <p>{{ project.detail }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      showDetail: false,
+    };
+  },
+};
+</script>
+
+<style>
+.project {
+  margin: 20px auto;
+  background: #fff;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+  border-left: 4px solid #e90074;
+}
+h3 {
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import data from "../../data/db.json";
+import SingleProject from "@/components/SingleProject.vue";
+export default {
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    this.projects = data.projects;
+  },
+};
+</script>
